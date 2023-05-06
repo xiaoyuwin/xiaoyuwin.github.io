2e9f615c402da044bca1500d82c0a7fe60240539
interaction2023 <127021574+xiaoyuwin@users.noreply.github.com>
2023-05-07 01:35:19 +0800

    Add files via upload

---
+++ homework.js
@@ -0,0 +1,41 @@
+function answer1(){
+  var response = prompt("答案格式是：xx(名字)，xx高中");
+  if (response=="山口忠，乌野高中"){
+    alert("恭喜答对！");
+}
+else {
+  alert("错啦");
+}
+}
+function answer2(){
+  var response = prompt("答案格式是：xx(名字)，xx高中");
+  if (response=="五色工，白鸟泽高中"){
+    alert("恭喜答对！");
+}
+else {
+  alert("错啦");
+}
+}
+function answer3(){
+  var response = prompt("答案格式是：xx(名字)，xx高中");
+  if (response=="孤爪研磨，音驹高中"){
+    alert("恭喜答对！");
+}
+else {
+  alert("错啦");
+}
+}
+function answer4(){
+  var response = prompt("答案格式是：xx(名字)，xx高中");
+  if (response=="赤苇京治，枭谷高中"){
+    alert("恭喜答对！");
+}
+else {
+  alert("错啦");
+}
+}
+
+function rightanswer(){
+alert
+("答案分别是：山口忠，乌野高中；五色工，白鸟泽高中；孤爪研磨，音驹高中；赤苇京治，枭谷高中。");
+          }
